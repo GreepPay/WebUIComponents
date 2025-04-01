30b8cc37a4307ef3ff18de07f384fc39ada4e010
Ayomide Akinola <drayfocus@gmail.com>
2025-04-01 08:30:10 +0100

    fix: button

--- src/components/AppButton/index.vue
+++ src/components/AppButton/index.vue
@@ -33,13 +33,30 @@
     ]"
     :disabled="disabled || loading"
   >
-    <div v-if="loading" class="absolute left-1/2 -translate-x-1/2">
-      <!-- <img
-        src="@/assets/svg/All/linear/refresh-2.svg"
-        class="w-5 h-5 animate-spin"
-        :class="{'opacity-40': disabled}"
-      /> -->
-    </div>
+    <span v-if="loading">
+      <svg
+        :class="`animate-spin mr-3 h-5 w-5  ${
+          type == 'light' ? 'text-white' : 'text-white'
+        }`"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </span>
     <template v-if="!loading">
       <slot name="icon" />
       <slot v-if="!iconOnly" />
